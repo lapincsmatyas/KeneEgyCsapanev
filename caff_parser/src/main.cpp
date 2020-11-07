5c8fc789593d8458569418b1c6826841f6bec463
Lapincs Mátyás <lapincs.matyas@gmail.com>
2020-11-07 19:52:45 +0100

    additional dependency removed

--- caff_parser/src/main.cpp
+++ caff_parser/src/main.cpp
@@ -1,9 +1,20 @@
 #include <iostream>
 #include <fstream>
 #include <cstring>
-#include "ciff.h"
+#include <vector>
 
 using namespace std;
+
+class Ciff {
+public:
+    unsigned int header_size{0};
+    unsigned int content_size{0};
+    unsigned int width{0};
+    unsigned int height{0};
+    std::string caption{""};
+    std::string tags{""};
+    std::vector<char> pixels{0};
+};
 
 Ciff ciff{};
 
