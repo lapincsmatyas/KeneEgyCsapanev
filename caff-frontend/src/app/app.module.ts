227c7e9877e1301c349be0798dd4915a1b8e66b9
Komphone <5258>
2020-11-22 12:54:58 +0100

    add frontend with auth

---
+++ caff-frontend/src/app/app.module.ts
@@ -0,0 +1,58 @@
+import { BrowserModule } from '@angular/platform-browser';
+import {  NgModule } from '@angular/core';
+import { HttpClientModule } from "@angular/common/http";
+import { RouterModule, Routes } from "@angular/router";
+import { FormsModule, ReactiveFormsModule} from "@angular/forms";
+import {MatToolbarModule} from '@angular/material/toolbar';
+import { MatButtonModule} from "@angular/material/button";
+import {MatIconModule} from "@angular/material/icon"
+
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { HomeComponent } from './components/home/home.component';
+import { LoginComponent } from './components/login/login.component';
+import { HeaderComponent } from './components/header/header.component';
+import { LogoutComponent } from './logout/logout.component';
+
+import { RegisterComponent } from './register/register.component';
+import { authInterceptorProviders } from './helpers/auth.interceptor';
+
+const routers: Routes = [
+  {path: 'header', component: HeaderComponent},
+  { path: 'home', component: HomeComponent},
+  { path: 'login', component: LoginComponent},
+  { path: 'register', component: RegisterComponent },
+  { path: 'logout', component: LogoutComponent},
+  // otherwise redirect to home
+  { path: '**', redirectTo: 'home' }
+];
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HeaderComponent,
+    HomeComponent,
+    LoginComponent,
+    RegisterComponent,
+    LogoutComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    HttpClientModule,
+    RouterModule.forRoot(routers),
+    FormsModule,
+    BrowserAnimationsModule,
+    MatToolbarModule,
+    MatButtonModule,
+    MatIconModule,
+    ReactiveFormsModule,
+    MatButtonModule,
+  ],
+  providers: [authInterceptorProviders],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
+
